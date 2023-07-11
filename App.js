4e18b92eb758daaf78ae2b22e56b9e30d099190c
Mbali <mbalikhumalo408@gmail.com>
2023-07-11 11:04:28 +0200

    Happy

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 
-import {StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
+
 
 export default function App() {
   return (
@@ -11,7 +12,14 @@ export default function App() {
       <View style={styles.main}>
         <Text style={{}}>HAPPY BIRTHDAY</Text>
       </View>
-      <Button title="Open Me"></Button>
+      <View style={styles.main}>
+        <Text >You are the princess and the fairytale of my life.
+          From the second I held you in my arms and looked into your eyes,
+          I knew you'd be my special little girl. I knew you'd bring me love and hope and joy,
+          and I was right.
+          You've been an angel. Happy Birthday! Love.</Text>
+      </View>
+      {/* <Button title="Open Me"></Button> */}
     </View>
   );
 }
@@ -22,13 +30,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'cyan'
   },
   main: {
     margin: 16,
-    borderWidth: 2,
-    borderColor: 'blue',
     padding: 10,
   }
 });
-
-
